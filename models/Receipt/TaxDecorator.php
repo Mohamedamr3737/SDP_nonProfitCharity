4c19bce7d410b219d7263a6d97055973d9dda8f8
SalmaNasreldin1 <114088774+SalmaNasreldin1@users.noreply.github.com>
2024-11-17 15:42:05 +0200

    Receipt with decorator pattern

---
+++ models/Receipt/TaxDecorator.php
@@ -0,0 +1,26 @@
+<?php
+
+// File: models/Receipt/TaxDecorator.php
+require_once 'Receipt.php';
+
+class TaxDecorator implements Receipt {
+    private $receipt;
+    private $taxRate;
+
+    public function __construct(Receipt $receipt, $taxRate=null) {
+        $this->receipt = $receipt;
+        $this->taxRate = $taxRate;
+    }
+
+    public function getDescription(): string {
+        return $this->receipt->getDescription() . " (including tax)";
+    }
+
+    public function getTotalAmount(): float {
+        // Calculate the tax amount and add it to the original total
+        $taxAmount = $this->receipt->getTotalAmount() * $this->taxRate;
+        return $this->receipt->getTotalAmount() + $taxAmount;
+    }
+}
+
+?>
